2cce2eb83b68e86778a45369b56c4be0700432ae
edlibra <exavieredlyn@gmail.com>
2026-04-15 21:21:00 -0300

    trying to fix bugs on deploy 1

--- edlynexavier/settings.py
+++ edlynexavier/settings.py
@@ -162,7 +162,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # ─────────────────────────────────────────────────────────────────────────────
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
